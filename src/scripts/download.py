48b94ca0227496d2f40048171c4c0db639768ae9
andrinahoffman <159974444+andrinahoffman@users.noreply.github.com>
2024-02-17 20:17:37 -0800

    Create download.py

---
+++ src/scripts/download.py
@@ -0,0 +1,22 @@
+import requests as req
+import csv
+
+# Function to fetch and save data for a given year
+def fetch_and_save_data(year):
+    '''
+    go to the given year and download the data from forbes
+    year: integer of the year
+    return: void
+    '''
+    url = f"https://www.forbes.com/ajax/list/data?year={year}&uri=billionaires&type=person"
+    response = req.get(url)
+    data = response.json()
+    headers = ['position', 'rank', 'name', 'lastName', 'uri', 'imageUri', 'worth', 'salary', 'managementAssets', 'government', 'title', 'pay', 'headquarters', 'state', 'age', 'source', 'industry', 'gender', 'country', 'timestamp', 'squareImage', 'worthChange', 'realTimeWorth', 'realTimeRank', 'realTimePosition']
+
+    with open(f'src/data/billionaires_{year}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for billionaire in data:
+            writer.writerow(billionaire)
+    csvfile.close()
+    print(f"Billionaires data for {year} saved successfully.")
