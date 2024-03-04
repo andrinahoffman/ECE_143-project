712dfc46b8258a5ebe3f3efbb1c44d51907ca4ac
andrinahoffman <159974444+andrinahoffman@users.noreply.github.com>
2024-03-04 02:03:48 -0500

    Update analyze.py
    adding the graphs to this file and adding more functionality

--- src/scripts/analyze.py
+++ src/scripts/analyze.py
@@ -1,3 +1,46 @@
+# from .visualizations.scatter_usa import plot_usa_map_with_data
+# from .visualizations.scatter_world import plot_world_map_with_data
+from .visualizations.bar_graph import get_bar_graph
+
+def display_all_graphs(allYearData, year):    
+    # countriesDf = allYearData[0]
+    # statesDf = allYearData[1]
+    # genderDf = allYearData[2]
+    # ageDf = allYearData[3]
+    industryDf = allYearData[4]
+    # sourceDf = allYearData[5]
+    # nameDf = allYearData[6]
+
+    # bar graph for gender
+    # get_bar_graph(genderDf, year)
+
+    # bar graph for age
+    # get_bar_graph(ageDf, year)
+
+
+    # bar graph for industry
+    get_bar_graph(industryDf, year)
+
+    # bar graph for source
+    # get_bar_graph(sourceDf.head(30), year)
+
+    # make bar graphs of the first letter of their name and the first letter of their last name
+    # first_name_table = get_first_name_table(nameDf)
+    # get_bar_graph(first_name_table, year)
+    # last_name_table = get_last_name_table(nameDf)
+    # get_bar_graph(last_name_table, year)
+
+    # make display hot locations in the world and usa
+    # plot_world_map_with_data(countriesDf)
+    # plot_usa_map_with_data(statesDf)
+
+    # showing what areas are the most important
+    # ns_table = get_north_south_state_table(statesDf)
+    # get_bar_graph(ns_table, year)
+    # ew_table = get_east_west_state_table(statesDf)
+    # get_bar_graph(ew_table, year)
+
+
 def parse_data_count_based_on_col(df, col):
     '''
     this will make a new dataframe that counts the occurrence for the column given
@@ -8,16 +51,35 @@ def parse_data_count_based_on_col(df, col):
     df_source_counts = df.groupby(col).size().reset_index(name='#_of_people').sort_values(by='#_of_people', ascending=False)
     return df_source_counts
 
+def build_table(df, col, new_col, assigment_method):
+    df[new_col] = df[col].apply(assigment_method)
+    table = df.pivot_table(index=new_col, values='#_of_people', aggfunc='sum')
+    table.reset_index(inplace=True)
+    return table
+
+def assign_based_on_first_letter_first_name(name):
+    return name[0]
+
+def assign_based_on_first_letter_last_name(name):
+    n = name.split()
+    if len(n)>1:
+        return n[1][0]
+    else:
+        return n[0][0]
+
+def get_first_name_table(df):
+    return build_table(df, 'name', 'first_letter_first_name', assign_based_on_first_letter_first_name)
+
+def get_last_name_table(df):
+    return build_table(df, 'name', 'first_letter_last_name', assign_based_on_first_letter_last_name)
+
 northern_states = ['Washington', 'Oregon', 'Idaho', 'Montana', 'North Dakota', 'South Dakota', 'Minnesota', 'Wisconsin', 'Michigan', 'Wyoming', 'Nebraska', 'Iowa', 'Illinois', 'Indiana', 'Ohio', 'Pennsylvania', 'New York', 'Vermont', 'New Hampshire', 'Maine', 'Massachusetts', 'Connecticut', 'Rhode Island', 'Alaska']
 southern_states = ['California', 'Nevada', 'Utah', 'Colorado', 'Kansas', 'Missouri', 'Kentucky', 'West Virginia', 'Virginia', 'Maryland', 'Delaware', 'New Jersey', 'North Carolina', 'South Carolina', 'Tennessee', 'Arkansas', 'Oklahoma', 'Texas', 'Louisiana', 'Mississippi', 'Alabama', 'Georgia', 'Florida', 'Arizona', 'New Mexico', 'Hawaii']
 
-def get_vertical_table(statesDf):
-    statesDf['vertical_regions'] = statesDf['state'].apply(assign_vertical_region)
-    vertical_table = statesDf.pivot_table(index='vertical_regions', values='#_of_people', aggfunc='sum')
-    vertical_table.reset_index(inplace=True)
-    return vertical_table
+def get_north_south_state_table(statesDf):
+    return build_table(statesDf, 'state', 'vertical_regions', assign_north_south_state_region)
 
-def assign_vertical_region(state):
+def assign_north_south_state_region(state):
     if state in northern_states:
         return 'North'
     elif state in southern_states:
@@ -30,13 +92,10 @@ midwest_states = ['North Dakota', 'South Dakota', 'Nebraska', 'Kansas', 'Minneso
 mid_east_states = ['Texas', 'Oklahoma', 'Arkansas', 'Louisiana', 'Kentucky', 'Tennessee', 'Mississippi', 'Alabama']
 east_states = ['Maine', 'New Hampshire', 'Vermont', 'Massachusetts', 'Rhode Island', 'Connecticut', 'New York', 'Pennsylvania', 'New Jersey', 'Delaware', 'Maryland', 'West Virginia', 'Virginia', 'North Carolina', 'South Carolina', 'Georgia', 'Florida']
 
-def get_horizontal_table(statesDf):
-    statesDf['horizontal_regions'] = statesDf['state'].apply(assign_horizontal_region)
-    horizontal_table = statesDf.pivot_table(index='horizontal_regions', values='#_of_people', aggfunc='sum')
-    horizontal_table.reset_index(inplace=True)
-    return horizontal_table
+def get_east_west_state_table(statesDf):
+    return build_table(statesDf, 'state', 'horizontal_regions', assign_east_west_state_region)
 
-def assign_horizontal_region(state):
+def assign_east_west_state_region(state):
     if state in west_states:
         return 'West'
     elif state in midwest_states:
@@ -47,3 +106,5 @@ def assign_horizontal_region(state):
         return 'East'
     else:
         return 'International'
+
+
