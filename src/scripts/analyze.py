6b0d8ea72a4b8505538716c96b394b0c66e13a36
andrinahoffman <159974444+andrinahoffman@users.noreply.github.com>
2024-03-06 15:31:36 -0800

    Update analyze.py

--- src/scripts/analyze.py
+++ src/scripts/analyze.py
@@ -1,66 +1,107 @@
 # from .visualizations.scatter_usa import plot_usa_map_with_data
 # from .visualizations.scatter_world import plot_world_map_with_data
-from .visualizations.bar_graph import get_bar_graph
+# from .visualizations.bar_graph import get_bar_graph
 
-def display_all_graphs(allYearData, year):    
+def display_all_graphs(allYearData, dfs):    
+    """
+    Display all required graphs based on the data.
+    
+    allYearData: List of DataFrames containing data for each year.
+    dfs: Dictionary to store categorized DataFrames.
+    
+    return: Dictionary containing categorized DataFrames.
+    """
     # countriesDf = allYearData[0]
     # statesDf = allYearData[1]
-    # genderDf = allYearData[2]
-    # ageDf = allYearData[3]
-    industryDf = allYearData[4]
-    # sourceDf = allYearData[5]
-    # nameDf = allYearData[6]
-
-    # bar graph for gender
-    # get_bar_graph(genderDf, year)
-
-    # bar graph for age
-    # get_bar_graph(ageDf, year)
-
-
-    # bar graph for industry
-    get_bar_graph(industryDf, year)
-
-    # bar graph for source
-    # get_bar_graph(sourceDf.head(30), year)
-
-    # make bar graphs of the first letter of their name and the first letter of their last name
-    # first_name_table = get_first_name_table(nameDf)
-    # get_bar_graph(first_name_table, year)
-    # last_name_table = get_last_name_table(nameDf)
-    # get_bar_graph(last_name_table, year)
-
     # make display hot locations in the world and usa
     # plot_world_map_with_data(countriesDf)
     # plot_usa_map_with_data(statesDf)
+    for i in range(len(allYearData)):
+        df_dict = determine_df(allYearData[i])
+        for key, value in df_dict.items():
+            if len(dfs.get(key, [])) > 0: 
+                dfs[key].append(value)
+            else:
+                dfs[key] = [value]
+    return dfs
 
-    # showing what areas are the most important
-    # ns_table = get_north_south_state_table(statesDf)
-    # get_bar_graph(ns_table, year)
-    # ew_table = get_east_west_state_table(statesDf)
-    # get_bar_graph(ew_table, year)
-
+def determine_df(df):
+    """
+    Determine the category of the DataFrame and return it.
+    
+    df: DataFrame to categorize.
+    
+    return: Dictionary containing categorized DataFrame.
+    """
+    col_name = df.columns[0]
+    match col_name:
+        case 'gender':
+            return {col_name: df}
+        case 'age':
+            return {col_name: df}
+        case 'industry':
+            return {col_name: df.head(30)}
+        case 'source':
+            return {col_name: df.head(30)}
+        case 'state':
+            return {
+                'states east west': get_east_west_state_table(df),
+                'states north south': get_north_south_state_table(df)
+            }
+        case 'name':
+            return {
+                "first name first letter": get_first_name_table(df),
+                "last name first letter": get_last_name_table(df)
+            }
+        case _:
+            return {col_name: df}
 
 def parse_data_count_based_on_col(df, col):
     '''
-    this will make a new dataframe that counts the occurrence for the column given
-    df: dataframe of all data
-    col: column to count
-    return: dataframe
+    Create a new DataFrame that counts the occurrence for the given column.
+    
+    df: DataFrame of all data.
+    col: Column to count.
+    
+    return: DataFrame.
     '''
     df_source_counts = df.groupby(col).size().reset_index(name='#_of_people').sort_values(by='#_of_people', ascending=False)
     return df_source_counts
 
-def build_table(df, col, new_col, assigment_method):
-    df[new_col] = df[col].apply(assigment_method)
+def build_table(df, col, new_col, assignment_method):
+    """
+    Build a pivot table from DataFrame based on specific column and assignment method.
+    
+    df: DataFrame.
+    col: Column to assign values from.
+    new_col: New column name.
+    assignment_method: Method to assign values.
+    
+    return: DataFrame.
+    """
+    df[new_col] = df[col].apply(assignment_method)
     table = df.pivot_table(index=new_col, values='#_of_people', aggfunc='sum')
     table.reset_index(inplace=True)
     return table
 
 def assign_based_on_first_letter_first_name(name):
+    """
+    Assign based on the first letter of the first name.
+    
+    name: Name string.
+    
+    return: First letter.
+    """
     return name[0]
 
 def assign_based_on_first_letter_last_name(name):
+    """
+    Assign based on the first letter of the last name.
+    
+    name: Name string.
+    
+    return: First letter.
+    """
     n = name.split()
     if len(n)>1:
         return n[1][0]
@@ -68,18 +109,46 @@ def assign_based_on_first_letter_last_name(name):
         return n[0][0]
 
 def get_first_name_table(df):
-    return build_table(df, 'name', 'first_letter_first_name', assign_based_on_first_letter_first_name)
+    """
+    Get a pivot table based on the first letter of the first name.
+    
+    df: DataFrame.
+    
+    return: DataFrame.
+    """
+    return build_table(df, 'name', 'first name first letter', assign_based_on_first_letter_first_name)
 
 def get_last_name_table(df):
-    return build_table(df, 'name', 'first_letter_last_name', assign_based_on_first_letter_last_name)
+    """
+    Get a pivot table based on the first letter of the last name.
+    
+    df: DataFrame.
+    
+    return: DataFrame.
+    """
+    return build_table(df, 'name', 'last name first letter', assign_based_on_first_letter_last_name)
 
 northern_states = ['Washington', 'Oregon', 'Idaho', 'Montana', 'North Dakota', 'South Dakota', 'Minnesota', 'Wisconsin', 'Michigan', 'Wyoming', 'Nebraska', 'Iowa', 'Illinois', 'Indiana', 'Ohio', 'Pennsylvania', 'New York', 'Vermont', 'New Hampshire', 'Maine', 'Massachusetts', 'Connecticut', 'Rhode Island', 'Alaska']
 southern_states = ['California', 'Nevada', 'Utah', 'Colorado', 'Kansas', 'Missouri', 'Kentucky', 'West Virginia', 'Virginia', 'Maryland', 'Delaware', 'New Jersey', 'North Carolina', 'South Carolina', 'Tennessee', 'Arkansas', 'Oklahoma', 'Texas', 'Louisiana', 'Mississippi', 'Alabama', 'Georgia', 'Florida', 'Arizona', 'New Mexico', 'Hawaii']
 
 def get_north_south_state_table(statesDf):
-    return build_table(statesDf, 'state', 'vertical_regions', assign_north_south_state_region)
+    """
+    Get a pivot table based on the northern and southern states.
+    
+    statesDf: DataFrame containing states data.
+    
+    return: DataFrame.
+    """
+    return build_table(statesDf, 'state', 'states north south', assign_north_south_state_region)
 
 def assign_north_south_state_region(state):
+    """
+    Assign states to the north or south region.
+    
+    state: State name.
+    
+    return: Region.
+    """
     if state in northern_states:
         return 'North'
     elif state in southern_states:
@@ -93,9 +162,23 @@ mid_east_states = ['Texas', 'Oklahoma', 'Arkansas', 'Louisiana', 'Kentucky', 'Te
 east_states = ['Maine', 'New Hampshire', 'Vermont', 'Massachusetts', 'Rhode Island', 'Connecticut', 'New York', 'Pennsylvania', 'New Jersey', 'Delaware', 'Maryland', 'West Virginia', 'Virginia', 'North Carolina', 'South Carolina', 'Georgia', 'Florida']
 
 def get_east_west_state_table(statesDf):
-    return build_table(statesDf, 'state', 'horizontal_regions', assign_east_west_state_region)
+    """
+    Get a pivot table based on the eastern and western states.
+    
+    statesDf: DataFrame containing states data.
+    
+    return: DataFrame.
+    """
+    return build_table(statesDf, 'state', 'states east west', assign_east_west_state_region)
 
 def assign_east_west_state_region(state):
+    """
+    Assign states to the east or west region.
+    
+    state: State name.
+    
+    return: Region.
+    """
     if state in west_states:
         return 'West'
     elif state in midwest_states:
@@ -106,5 +189,3 @@ def assign_east_west_state_region(state):
         return 'East'
     else:
         return 'International'
-
-
